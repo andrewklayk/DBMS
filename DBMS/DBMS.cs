15e0c9bcac2b8046ef0d407d0c9262f417ebea6c
Andrew <andrew.klyachkin@gmail.com>
2020-09-28 12:23:41 +0300

    Add project files.

---
+++ DBMS/DBMS.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace DBMS
+{
+    class Database
+    {
+        private string connectionString;
+        Database(DBParams dBParams)
+        {
+            connectionString = BuildConnectionString(dBParams);
+        }
+        public void CreateDatabase(DBParams dbParams, DBParams masterParams)
+        {
+            SqlConnection con = new SqlConnection();
+            string sqlCreateDbQuery;
+            con.ConnectionString = BuildConnectionString(masterParams);
+            sqlCreateDbQuery = $" CREATE DATABASE {dbParams.DBName} " +
+                $"ON PRIMARY (NAME = {dbParams.DBFileName}, FILENAME = {dbParams.DBFilePath}) " +
+                $"LOG ON (NAME = {dbParams.LogFileName}, FILENAME = {dbParams.LogFilePath})";
+            SqlCommand createCommand = new SqlCommand(sqlCreateDbQuery, con);
+            try
+            {
+                con.Open();
+                createCommand.ExecuteNonQuery();
+                MessageBox.Show($"Database {dbParams.DBName} created succesfully!", "DBMS", MessageBoxButtons.OK);
+            }
+            catch(System.Exception e)
+            {
+                MessageBox.Show(e.ToString(), "DBMS", MessageBoxButtons.OK);
+            }
+            finally
+            {
+                con.Close();
+            }
+            return;
+        }
+        private string BuildConnectionString(DBParams dbParams)
+        {
+            return $"SERVER = {dbParams.ServerName}; DATABASE = {dbParams.DBName}; User ID ={dbParams.UserId}; Pwd = {dbParams.Password}"; ;
+        }
+    }
+}
---
+++ DBMS/DBParams.cs
@@ -0,0 +1,14 @@
+﻿namespace DBMS
+{
+    struct DBParams
+    {
+        public string ServerName { get; set; }
+        public string DBName { get; set; }
+        public string DBFileName { get; set; }
+        public string DBFilePath { get; set; }
+        public string LogFileName { get; set; }
+        public string LogFilePath { get; set; }
+        public string UserId { get; set; }
+        public string Password { get; set; }
+    }
+}
